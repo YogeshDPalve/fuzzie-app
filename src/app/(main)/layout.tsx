41e3e4d42b6cab4271279a5331b15e1787a8f1cb
Yogesh Palve <dnyyogesh@gmail.com>
2025-02-20 21:37:22 +0530

    layout for pages generated

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,11 +1,13 @@
-import React from 'react'
+import React from "react";
 
-type Props = {}
+type Props = { children: React.ReactNode };
 
 const Layout = (props: Props) => {
   return (
-	<div>Layout</div>
-  )
-}
+    <div className="flex overflow-hidden h-screen">
+      <div className="w-full">{props.children}</div>
+    </div>
+  );
+};
 
-export default Layout
+export default Layout;
