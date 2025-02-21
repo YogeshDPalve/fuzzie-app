e77bb36ef39873cb30e98fb463389e313908c50d
Yogesh Palve <dnyyogesh@gmail.com>
2025-02-21 21:54:29 +0530

    .env.example file created with all requirements

---
+++ src/app/(main)/(pages)/settings/page.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+type Props = {}
+
+const Settings = (props: Props) => {
+  return (
+	  <div className='flex flex-col gap-4'>
+		  <h1>
+			  
+		</h1>
+		  
+	</div>
+  )
+}
+
+export default Settings
