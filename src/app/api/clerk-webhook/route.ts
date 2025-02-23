cccd42437cee40b2e3d336ee0e521291408a391d
Yogesh Palve <dnyyogesh@gmail.com>
2025-02-23 21:14:55 +0530

    new user creation route added

--- src/app/api/clerk-webhook/route.ts
+++ src/app/api/clerk-webhook/route.ts
@@ -6,7 +6,7 @@ export async function POST(req: Request) {
     const body = await req.json();
     const { id, email_addresses, first_name, image_url } = body?.data;
 
-    const email = email_addresses[0]?.email_addresses;
+    const email = email_addresses[0]?.email_address;
     console.log("✅", body);
 
     await db.user.upsert({
@@ -15,7 +15,7 @@ export async function POST(req: Request) {
         email,
         name: first_name,
         profileImage: image_url,
-      },
+      },  
       create: {
         clerkId: id,
         email,
@@ -23,14 +23,11 @@ export async function POST(req: Request) {
         profileImage: image_url || "",
       },
     });
-
-    return new NextResponse("User Updated in Database Successfully", {
+    return new NextResponse("User updated in database successfully", {
       status: 200,
     });
   } catch (error) {
-    console.error("Error updating database", error);
-    return new NextResponse("Error updating database", {
-      status: 500,
-    });
+    console.error("Error updating database:", error);
+    return new NextResponse("Error updating user in database", { status: 500 });
   }
 }
