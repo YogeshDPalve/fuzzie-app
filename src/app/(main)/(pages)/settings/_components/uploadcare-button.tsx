5d854c103cfd9da1520bb42b79f67cbd9535e74e
Yogesh Palve <dnyyogesh@gmail.com>
2025-02-24 21:33:26 +0530

    uploadcare upload Images bug fixed

--- src/app/(main)/(pages)/settings/_components/uploadcare-button.tsx
+++ src/app/(main)/(pages)/settings/_components/uploadcare-button.tsx
@@ -3,12 +3,14 @@ import React from "react";
 import { FileUploaderRegular } from "@uploadcare/react-uploader/next";
 import "@uploadcare/react-uploader/core.css";
 import { useTheme } from "next-themes";
+import { uploadFile } from "@uploadcare/upload-client";
 type Props = {
   onUpload?: any;
 };
 
-const UploadcareButton = (props: Props) => {
+const UploadcareButton = ({ onUpload }: Props) => {
   const { resolvedTheme } = useTheme();
+
   return (
     <div>
       {" "}
@@ -19,6 +21,12 @@ const UploadcareButton = (props: Props) => {
           resolvedTheme === "dark" ? "uc-dark uc-purple" : "uc-light uc-purple"
         }
         pubkey="1632966e247fec39762f"
+        onFileUploadSuccess={(event) => {
+          if (event.fullPath && onUpload) {
+            console.log(event.fullPath);
+            onUpload(event.fullPath); // Send the uploaded file URL
+          }
+        }}
       />
     </div>
   );
--- src/app/(main)/(pages)/settings/page.tsx
+++ src/app/(main)/(pages)/settings/page.tsx
@@ -29,9 +29,23 @@ const Settings = async (props: Props) => {
       where: { clerkId: authUser.id },
       data: { profileImage: image },
     });
+    
     return responce;
   };
 
+  const updateUserInfo = async (name: string) => {
+    "use server";
+
+    const updateUser = await db.user.update({
+      where: {
+        clerkId: authUser.id,
+      },
+      data: {
+        name,
+      },
+    });
+    return updateUser;
+  };
   return (
     <div className="flex flex-col gap-4">
       <h1 className="sticky top-0 z-[10] flex items-center justify-between border-b bg-background/50 p-6 text-4xl backdrop-blur-lg">
